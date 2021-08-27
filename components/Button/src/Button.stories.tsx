15ccffbfb7d9b536f16c2dc4f39bee1345ee825b
Franck <franck.gaudin@gsoft.com>
2021-08-27 17:15:03 -0400

    chore: button component first commit

--- components/Button/src/Button.stories.tsx
+++ components/Button/src/Button.stories.tsx
@@ -1,9 +1,103 @@
-import React, { ReactElement } from 'react';
+import * as React from 'react';
+import { Meta } from '@storybook/react';
 
 import Button from './Button';
 
 export default {
   title: 'Components/Button',
-};
+  component: Button,
+  decorators: [
+    (Story) => (
+      <div style={{ padding: '3rem' }}>
+        <Story />
+      </div>
+    ),
+  ],
+} as Meta;
 
-export const BasicUsage = (): ReactElement => <Button />;
+// eslint-disable-next-line react/prop-types
+const Grid: React.FunctionComponent = ({ children }): JSX.Element => (
+  <div className="isb-grid">{children}</div>
+);
+
+export const BasicUsage = (): React.ReactElement => (
+  <>
+    <h1>Buttons</h1>
+    <section className="isb-section">
+      <h2 className="isb-section__title">Primary</h2>
+      <Grid>
+        <h3 className="isb-section__subtitle">Default</h3>
+        <Button dataTest="test" size="small">
+          Click me
+        </Button>
+        <Button>Click me</Button>
+        <h3 className="isb-section__subtitle">Disabled</h3>
+        <Button disabled size="small">
+          Click me
+        </Button>
+        <Button disabled>Click me</Button>
+      </Grid>
+    </section>
+
+    <section className="isb-section">
+      <h2 className="isb-section__title">Secondary</h2>
+      <Grid>
+        <h3 className="isb-section__subtitle">Default</h3>
+        <Button appearance="secondary" size="small">
+          Click me
+        </Button>
+        <Button appearance="secondary">Click me</Button>
+        <h3 className="isb-section__subtitle">Disabled</h3>
+        <Button appearance="secondary" disabled size="small">
+          Click me
+        </Button>
+        <Button appearance="secondary" disabled>
+          Click me
+        </Button>
+        <h3 className="isb-section__subtitle">Actived</h3>
+        <Button appearance="secondary" active size="small">
+          Click me
+        </Button>
+        <Button appearance="secondary" active>
+          Click me
+        </Button>
+      </Grid>
+    </section>
+
+    <section className="isb-section">
+      <h2 className="isb-section__title">Danger</h2>
+      <Grid>
+        <h3 className="isb-section__subtitle">Default</h3>
+        <Button appearance="danger" size="small">
+          Click me
+        </Button>
+        <Button appearance="danger">Click me</Button>
+        <h3 className="isb-section__subtitle">Disabled</h3>
+        <Button appearance="danger" disabled size="small">
+          Click me
+        </Button>
+        <Button appearance="danger" disabled>
+          Click me
+        </Button>
+      </Grid>
+    </section>
+
+    <section className="isb-section">
+      <h2 className="isb-section__title">Ghost</h2>
+      <Grid>
+        <h3 className="isb-section__subtitle">Default</h3>
+        <Button appearance="ghost" size="small">
+          Click me
+        </Button>
+        <Button appearance="ghost">Click me</Button>
+        <h3 className="isb-section__subtitle">Disabled</h3>
+        <Button appearance="ghost" disabled size="small">
+          Click me
+        </Button>
+        <Button appearance="ghost" disabled>
+          Click me
+        </Button>
+      </Grid>
+    </section>
+  </>
+);
--- components/Button/src/Button.tsx
+++ components/Button/src/Button.tsx
@@ -1,11 +1,44 @@
-import React, { ReactElement } from 'react';
+import * as React from 'react';
+import classNames from 'classnames';
 
 import './button.css';
 
-const Button = (): ReactElement => (
-  <button className="ids-btn" type="submit">
-    Button
-  </button>
-);
+export interface Props extends React.ComponentPropsWithoutRef<'button'> {
+  children: React.ReactNode;
+  disabled?: boolean;
+  active?: boolean;
+  size?: 'small' | 'medium';
+  appearance?: 'primary' | 'secondary' | 'ghost' | 'danger';
+  dataTest?: string;
+}
+
+const Button: React.FunctionComponent<Props> = (props: Props) => {
+  const {
+    children,
+    disabled = false,
+    active = false,
+    size = 'medium',
+    appearance = 'primary',
+    dataTest = undefined,
+    ...rest
+  } = props;
+
+  const classes = classNames(
+    'ids-btn',
+    size === 'small' && 'ids-btn--small',
+    active && 'ids-btn--active',
+    appearance !== 'primary' && `ids-btn--${appearance}`
+  );
+  return (
+    <button
+      disabled={disabled}
+      className={classes}
+      data-test={dataTest}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+};
 
 export default Button;
