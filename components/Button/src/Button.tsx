166cc2b08287cfebdbc5ff8167dd72e4207e790f
Franck <franck.gaudin@gsoft.com>
2021-09-02 14:56:36 -0400

    chore: button component developpment

--- components/Button/src/Button.tsx
+++ components/Button/src/Button.tsx
@@ -4,12 +4,28 @@ import classNames from 'classnames';
 import './button.css';
 
 export interface Props extends React.ComponentPropsWithoutRef<'button'> {
-  children: React.ReactNode;
+  // The content to dispaly inside the button
+  children?: React.ReactNode;
+  // Disabled the button, the user cannot click on them
   disabled?: boolean;
+  // Sets the button in a active state
   active?: boolean;
+  // Changes the size of button, giving more or less padding
   size?: 'small' | 'medium';
-  appearance?: 'primary' | 'secondary' | 'ghost' | 'danger';
+  // Button appearance
+  appearance?: 'primary' | 'secondary' | 'premium' | 'ghost' | 'danger';
+  // Add a data-test tag for automated tests
   dataTest?: string;
+  // Icon to display to the left of button content
+  iconLeading?: React.ReactNode;
+  // Icon to display to the right of button content
+  iconTrailing?: React.ReactNode;
+  // Callback when clicked
+  onClick?: () => void;
+  // Optional prop to specify the type of the Button
+  type?: 'button' | 'reset' | 'submit';
+  // Add a data-intercom-target with unique id to link a components to a Product Tour step.
+  intercomTarget?: string;
 }
 
 const Button: React.FunctionComponent<Props> = (props: Props) => {
@@ -19,24 +35,42 @@ const Button: React.FunctionComponent<Props> = (props: Props) => {
     active = false,
     size = 'medium',
     appearance = 'primary',
-    dataTest = undefined,
+    type = 'button',
+    onClick,
+    dataTest,
+    iconLeading,
+    iconTrailing,
+    intercomTarget,
     ...rest
   } = props;
+
+  const hasIconLeading = iconLeading !== undefined;
+  const hasIconTrailing = iconTrailing !== undefined;
+  const hasIcon = hasIconLeading || hasIconTrailing;
 
   const classes = classNames(
     'ids-btn',
     size === 'small' && 'ids-btn--small',
     active && 'ids-btn--active',
-    appearance !== 'primary' && `ids-btn--${appearance}`
+    appearance !== 'primary' && `ids-btn--${appearance}`,
+    hasIcon && 'has-icon',
+    hasIconLeading && 'has-icon--leading',
+    hasIconTrailing && 'has-icon--trailing'
   );
+
   return (
     <button
       disabled={disabled}
       className={classes}
       data-test={dataTest}
+      data-intercom-target={intercomTarget}
+      type={type}
+      onClick={onClick}
       {...rest}
     >
+      {hasIconLeading && iconLeading}
       {children}
+      {hasIconTrailing && iconTrailing}
     </button>
   );
 };
---
+++ typings/icons.d.ts
@@ -0,0 +1,4 @@
+declare module '@igloo-ui/icons/dist/Plus';
+declare module '@igloo-ui/icons/dist/Calendar';
+declare module '@igloo-ui/icons/dist/RemoveCalendar';
+declare module '@igloo-ui/icons/dist/Edit';
