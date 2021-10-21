00cde34c3b3ee2959e3d1fc98d14f8007e187381
Franck <franck.gaudin@gsoft.com>
2021-10-21 15:05:52 -0400

    fix: lint error

--- components/Kashim/rollup.config.js
+++ components/Kashim/rollup.config.js
@@ -1,6 +1,6 @@
 import path from 'path';
 import * as pkg from './package.json';
-import { createRollupConfig } from '../../rollup.config';
+import { createRollupConfig } from '../../rollup.config.js';
 
 const distDir = path.resolve(__dirname, './dist');
 const { name } = pkg;
