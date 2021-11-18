99ca7c9e492a14c74c94c78d7d05d6a882a488eb
Sylvester Vuong <sylvester.vuong@gsoft.com>
2021-11-18 13:45:50 -0500

    chore(storybook): add section with title

--- packages/Ellipsis/src/Ellipsis.stories.tsx
+++ packages/Ellipsis/src/Ellipsis.stories.tsx
@@ -4,21 +4,23 @@ import { Meta } from '@storybook/react';
 
 import Ellipsis from './Ellipsis';
 
+const ellipsisContent =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vestibulum ex vel felis commodo auctor. Nulla pulvinar velit eget scelerisque mattis. Curabitur augue leo, gravida non quam at, viverra varius metus. Mauris convallis, justo et fringilla rhoncus, nisi est tristique dui, ac consequat mauris erat sed lacus. Pellentesque interdum vel nibh non mattis. Donec vitae nulla massa. Vestibulum facilisis, lorem in egestas interdum, nibh ligula gravida nisl, nec interdum massa orci sit amet nisi. Sed fringilla vestibulum metus id auctor. Ut mollis auctor odio, eu consequat dui bibendum vitae. Sed aliquet luctus vestibulum.';
+
 export default {
   title: 'Components/Ellipsis',
   component: Ellipsis,
 } as Meta;
 
 export const Standard: React.VFC<unknown> = () => (
-  <Ellipsis>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vestibulum
-    ex vel felis commodo auctor. Nulla pulvinar velit eget scelerisque mattis.
-    Curabitur augue leo, gravida non quam at, viverra varius metus. Mauris
-    convallis, justo et fringilla rhoncus, nisi est tristique dui, ac consequat
-    mauris erat sed lacus. Pellentesque interdum vel nibh non mattis. Donec
-    vitae nulla massa. Vestibulum facilisis, lorem in egestas interdum, nibh
-    ligula gravida nisl, nec interdum massa orci sit amet nisi. Sed fringilla
-    vestibulum metus id auctor. Ut mollis auctor odio, eu consequat dui bibendum
-    vitae. Sed aliquet luctus vestibulum.
-  </Ellipsis>
+  <>
+    <section className="isb-section">
+      <h2>Default behaviour</h2>
+      <Ellipsis>{ellipsisContent}</Ellipsis>
+    </section>
+    <section className="isb-section">
+      <h2>With title</h2>
+      <Ellipsis title="This is a title">{ellipsisContent}</Ellipsis>
+    </section>
+  </>
 );
