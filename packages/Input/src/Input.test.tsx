8e6a4ec3b6387de3abb2fe97061fc592fe2fc129
Sylvester Vuong <sylvester.vuong@gmail.com>
2021-12-02 15:31:01 -0500

    Feature/input (#18)
    * feat(input): create component and css
    
    * build(input): add classnames to dependencies + add comments to props
    
    * docs(input): edit stories
    
    * fix(input): fix ref
    
    * refactor(input): organize css
    
    * chore(input): disable exhaustive deps
    
    * test(input): add tests
    
    * fix(input): missing replace
    
    * refactor(input): rename classname
    
    * docs(input): add changeset
    
    * fix(input): fix exhaustive-deps
    
    * fix(input): fix style lint
    
    * Apply suggestions from code review
    
    Co-authored-by: Franck Gaudin <franck.gaudin@gsoft.com>
    
    * Apply some PR changes
    
    * fix(input): lint
    
    * chore(input): change onKeyPress to onChange
    
    * docs(input): changeset
    
    * chore(input): additional css changes
    
    * docs(input): fix inputsize description in changeset
    
    * chore(input): add return type to test functions
    
    * refactor(input): change class name for error
    
    * refactor(input): cleanup css
    
    * refactor(input): change prop inputSize to isCompact
    
    * chore(input): remove useless const
    
    * test(input): fix test for size
    
    * chore(input): cleanup css
    
    * fix(input): fix focus style and add active
    
    * fix(input): rename label in storybook
    
    * refactor(input): use samoyed in css
    
    * chore(input): add error with focus
    
    * Apply suggestions from code review
    
    Co-authored-by: Franck Gaudin <franck.gaudin@gsoft.com>
    
    * refactor(input): change css for focus
    
    * refactor(input): change css to scss
    
    * chore(input): change to single quotes in scss
    
    Co-authored-by: Franck Gaudin <franck.gaudin@gsoft.com>

---
+++ packages/Input/src/Input.test.tsx
@@ -0,0 +1,60 @@
+/**
+ * @jest-environment jsdom
+ */
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Input from './Input';
+
+const setUp = (props = {}) => {
+  const component = shallow(<Input dataTest="ids-input" {...props} />);
+  return component;
+};
+
+describe('Input', () => {
+  test('It should render without errors', (): void => {
+    const input = setUp();
+    const wrapper = input.find('.ids-input');
+    expect(wrapper.length).toBe(1);
+  });
+
+  test('It should render a small size', (): void => {
+    const input = setUp({ isCompact: true });
+    const wrapper = input.find('.ids-input--compact');
+    expect(wrapper.length).toBe(1);
+  });
+
+  test('It should render a readonly', (): void => {
+    const input = setUp({ readOnly: true });
+    const wrapper = input.find('.ids-input--readonly');
+    expect(wrapper.length).toBe(1);
+  });
+
+  test('It should render a snapshot', (): void => {
+    const input = setUp();
+    expect(input).toMatchSnapshot();
+  });
+
+  test('It should render an email type', (): void => {
+    const input = setUp({ value: 'hashbrown@potatocatinc.com' });
+    expect(input).toMatchSnapshot();
+  });
+
+  test('It should render a text type', (): void => {
+    const input = setUp({ type: 'test', value: 'potat' });
+    expect(input).toMatchSnapshot();
+  });
+
+  test('It should render a password type', (): void => {
+    const input = setUp({
+      type: 'password',
+      value: 'this is a great password!',
+    });
+    expect(input).toMatchSnapshot();
+  });
+
+  test('It should render a number type', (): void => {
+    const input = setUp({ type: 'number', value: 12345 });
+    expect(input).toMatchSnapshot();
+  });
+});
