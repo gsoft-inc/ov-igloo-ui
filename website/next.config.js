19e28cb020ba75515288198ffa358a74dd3e6839
Franck <franck.gaudin@gsoft.com>
2021-12-03 15:48:13 -0500

    ci: set target serverless

--- website/next.config.js
+++ website/next.config.js
@@ -4,6 +4,7 @@ const path = require('path');
 module.exports = withTM({
   distDir: 'out',
   reactStrictMode: true,
+  target: 'serverless',
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
