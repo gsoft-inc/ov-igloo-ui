b47529db3abd82a4c51d63b8137d7995e2b05df0
Franck <franck.gaudin@gsoft.com>
2021-12-03 16:13:11 -0500

    ci: remove serverless config

--- website/next.config.js
+++ website/next.config.js
@@ -4,7 +4,6 @@ const path = require('path');
 module.exports = withTM({
   distDir: 'out',
   reactStrictMode: true,
-  target: 'serverless',
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
