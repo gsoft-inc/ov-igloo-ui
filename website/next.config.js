135b5fb9a382be521259c58ed321bac41b004fe6
Franck <franck.gaudin@gsoft.com>
2021-12-03 15:20:53 -0500

    fix: project command fail

--- website/next.config.js
+++ website/next.config.js
@@ -2,16 +2,9 @@ const withTM = require('next-transpile-modules')(['@igloo-ui/icons']);
 const path = require('path');
 
 module.exports = withTM({
+  distDir: 'out',
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
-  // webpack(config) {
-  //   config.module.rules.push({
-  //     test: /\.svg$/,
-  //     use: ['@svgr/webpack'],
-  //   });
-
-  //   return config;
-  // },
 });
