17343500e8d28b4b33353a52e02d20fb3586832a
Alexandre Beaudoin <ofrogon@gmail.com>
2022-01-17 16:15:17 -0500

    Feature/tooltip (#25)
    * Start importing from original code
    
    * Add .editorconfig file to set IDE as .eslintrc and fix lint warning
    
    * Correction de la position du Tooltip
    
    * Nettoyage rapide et Tooltip fonctionnel
    
    * Add fade-in animation
    
    * Ajout de ReactComponent comme body d'un tooltip
    
    * Changement de la demoReactComponent pour la rendre moins R.A.
    
    * Work on position fallback logic
    
    * Quick naming change
    
    * Fix linting in Tooltip
    
    * Fix Right and Bottom available space logic
    
    * Simplify position script in Tooltip
    
    * Add Tooltip tests
    
    * Suppression des fichiers transmis par erreur
    
    * chore: update code after code review
    
    * ci: Fix tests
    
    since Dark is now default theme, the tests looking for css classes needed to be updated
    
    * chore: update code after code review
    
    * chore: update code after code review (add missing fixes)
    
    * chore: update code after code review (add missing fixes)

---
+++ packages/Tooltip/src/Tooltip.stories.tsx
@@ -0,0 +1,129 @@
+import React from 'react';
+
+import { Meta } from '@storybook/react';
+
+import Tooltip, { TooltipProps } from './Tooltip';
+
+const tooltipContent = 'Lorem ipsum dolor';
+
+export default {
+  title: 'Components/Tooltip',
+  component: Tooltip,
+  argTypes: {
+    content: {
+      control: 'text',
+      defaultValue: tooltipContent,
+    },
+  },
+} as Meta;
+
+export const Playground: React.VFC<TooltipProps> = (args) => {
+  const displayContainerStyle = {
+    paddingTop: 40,
+    paddingBottom: 40,
+    paddingRight: 50,
+    paddingLeft: 60,
+    fontSize: '18px',
+  };
+
+  return (
+    <div style={displayContainerStyle} key="0">
+      <Tooltip {...args}>Playground tooltip</Tooltip>
+    </div>
+  );
+};
+
+interface TemplateProps {
+  payload: { dark: TooltipProps[]; light: TooltipProps[] };
+}
+
+const Template = ({ payload }: TemplateProps): React.ReactElement => {
+  const componentState = Object.keys(payload);
+  const { dark, light } = payload;
+
+  const component = componentState.map((state, index) => {
+    const isDark = state === 'dark';
+    const components = isDark ? dark : light;
+
+    const list = components.map((tooltipProps, i) => {
+      const displayContainerStyle = {
+        order: i,
+        padding: 20,
+        fontSize: '18px',
+      };
+
+      return (
+        <div style={displayContainerStyle} key={i.toString()}>
+          <Tooltip
+            appearance={isDark ? 'dark' : 'light'}
+            key={i.toString()}
+            {...tooltipProps}
+          >
+            {tooltipProps.children}
+          </Tooltip>
+        </div>
+      );
+    });
+
+    const title = state.charAt(0).toUpperCase() + state.slice(1);
+
+    return (
+      <section className="isb-section" key={index.toString()}>
+        <h2>{title}</h2>
+        <div className="isb-section__content">{list}</div>
+      </section>
+    );
+  });
+
+  return <>{component}</>;
+};
+
+const demoReactComponent = (
+  <div>
+    <span
+      style={{ color: '#30C9AD', fontSize: 12, textTransform: 'uppercase' }}
+    >
+      Strongly Favorable
+    </span>
+    <br />
+    <span style={{ fontWeight: 'bold', fontSize: 32 }}>
+      100,0<sup style={{ fontSize: 14 }}>%</sup>
+    </span>
+  </div>
+);
+
+const tooltipStates: TooltipProps[] = [
+  {
+    children: 'Default',
+    content: tooltipContent,
+  },
+  {
+    children: 'On Top',
+    content: tooltipContent,
+    position: 'top',
+  },
+  {
+    children: 'On the right',
+    content: tooltipContent,
+    position: 'right',
+  },
+  {
+    children: 'Underneath',
+    content: tooltipContent,
+    position: 'bottom',
+  },
+  {
+    children: 'On the left',
+    content: tooltipContent,
+    position: 'left',
+  },
+  {
+    children: 'With ReactComponent',
+    content: demoReactComponent,
+    position: 'bottom',
+  },
+];
+
+export const Standard = (): React.ReactElement => (
+  <Template payload={{ dark: tooltipStates, light: tooltipStates }} />
+);
