0df85302f05c2e990dcd5286e2d2741b0cc658d1
Alexandre Beaudoin <ofrogon@gmail.com>
2022-01-25 14:08:30 -0500

    feat: Apply changes to Button package only (#39)

--- packages/Button/src/Button.tsx
+++ packages/Button/src/Button.tsx
@@ -11,38 +11,38 @@ export type Appearance =
   | 'danger';
 export type Size = 'small' | 'medium';
 
-export interface Props extends React.ComponentPropsWithoutRef<'button'> {
-  // The content to display inside the button
+export interface ButtonProps extends React.ComponentPropsWithoutRef<'button'> {
+  /** The content to display inside the button */
   children?: React.ReactNode;
-  // Disabled the button, the user cannot click on them
+  /** Disabled the button, the user cannot click on them */
   disabled?: boolean;
-  // Sets the button in a active state
+  /** Sets the button in an active state */
   active?: boolean;
-  // Replaces button text with a spinner while a background action is being performed
+  /** Replaces button text with a spinner while a background action is being performed */
   loading?: boolean;
-  // Changes the size of button, giving more or less padding
+  /** Changes the size of button, giving more or less padding */
   size?: Size;
-  // Button appearance
+  /** Button appearance */
   appearance?: Appearance;
-  // Add a data-test tag for automated tests
+  /** Add a data-test tag for automated tests */
   dataTest?: string;
-  // Icon to display to the left of button content
+  /** Icon to display to the left of button content */
   iconLeading?: React.ReactNode;
-  // Icon to display to the right of button content
+  /** Icon to display to the right of button content */
   iconTrailing?: React.ReactNode;
-  // Display only the icon in mobile
+  /** Display only the icon in mobile */
   showOnlyIconOnMobile?: boolean;
-  // Callback when clicked
+  /** Callback when clicked */
   onClick?: () => void;
-  // Optional prop to specify the type of the Button
+  /** Optional prop to specify the type of the Button */
   type?: 'button' | 'reset' | 'submit';
-  // Add a data-intercom-target with unique id to link a components to a Product Tour step.
+  /** Add a data-intercom-target with unique id to link a components to a Product Tour step */
   intercomTarget?: string;
-  // Add a specific class to the button
+  /** Add a specific class to the button */
   className?: string;
 }
 
-const Button: React.FunctionComponent<Props> = (props: Props) => {
+const Button: React.FunctionComponent<ButtonProps> = (props: ButtonProps) => {
   const {
     children,
     disabled = false,
