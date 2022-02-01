12ff6b72719e7f0525373d65df90dc132e10e025
Franck <franck.gaudin@gsoft.com>
2022-02-01 16:30:19 -0500

    fix: ga lib

---
+++ website/lib/ga/index.js
@@ -0,0 +1,17 @@
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
+
+// log the pageview with their URL
+export const pageview = (url) => {
+  window.gtag('config', GA_TRACKING_ID, {
+    page_path: url,
+  });
+};
+
+// log specific events happening.
+export const event = ({ action, category, label, value }) => {
+  window.gtag('event', action, {
+    event_category: category,
+    event_label: label,
+    value: value,
+  });
+};
