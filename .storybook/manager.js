e03a7bf97d0beace421de0a702fa21ae53cd4b07
Franck <franck.gaudin@gsoft.com>
2022-02-02 10:14:16 -0500

    docs: add analitycs to storybook

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,10 +1,7 @@
 import { addons } from '@storybook/addons';
 import ovTheme from './theme/index';
 
-import * as ga from '../website/lib/ga';
-
-window.STORYBOOK_GA_ID = ga.GA_TRACKING_ID;
-window.STORYBOOK_REACT_GA_OPTIONS = {};
+window.STORYBOOK_GA_ID = process.env.NEXT_PUBLIC_GA_ID;
 
 addons.setConfig({
   theme: ovTheme,
