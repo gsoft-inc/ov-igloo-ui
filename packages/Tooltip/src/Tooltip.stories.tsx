bde96484a9a5c12b2b042321ccc7ab9b80699d10
Franck <franck.gaudin@gsoft.com>
2022-03-11 15:15:28 -0500

    chore: rewrite Toaster and Tooltip stories

--- packages/Tooltip/src/Tooltip.stories.tsx
+++ packages/Tooltip/src/Tooltip.stories.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
-import { Meta } from '@storybook/react';
+import { ComponentMeta } from '@storybook/react';
 
-import Tooltip, { TooltipProps } from './Tooltip';
+import Tooltip from './Tooltip';
 
+import Section from '@components/section';
 import readme from '../README.md';
 
 const tooltipContent = 'Lorem ipsum dolor';
@@ -19,116 +20,55 @@ export default {
       control: 'text',
       defaultValue: tooltipContent,
     },
+    position: { table: { defaultValue: { summary: 'top' } } },
+    appearance: { table: { defaultValue: { summary: 'dark' } } },
+    maxWidth: { table: { defaultValue: { summary: 200 } } },
   },
-} as Meta;
+  decorators: [
+    (Story) => (
+      <div
+        style={{
+          fontSize: '1.6rem',
+          paddingTop: 40,
+          paddingBottom: 40,
+          paddingRight: 50,
+          paddingLeft: 60,
+        }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+} as ComponentMeta<typeof Tooltip>;
 
-export const Playground: React.VFC<TooltipProps> = (args) => {
-  const displayContainerStyle = {
-    paddingTop: 40,
-    paddingBottom: 40,
-    paddingRight: 50,
-    paddingLeft: 60,
-    fontSize: '18px',
-  };
-
-  return (
-    <div style={displayContainerStyle} key="0">
-      <Tooltip {...args}>Playground tooltip</Tooltip>
-    </div>
-  );
-};
-
-interface TemplateProps {
-  payload: { dark: TooltipProps[]; light: TooltipProps[] };
-}
-
-const Template = ({ payload }: TemplateProps): React.ReactElement => {
-  const componentState = Object.keys(payload);
-  const { dark, light } = payload;
-
-  const component = componentState.map((state, index) => {
-    const isDark = state === 'dark';
-    const components = isDark ? dark : light;
-
-    const list = components.map((tooltipProps, i) => {
-      const displayContainerStyle = {
-        order: i,
-        padding: 20,
-        fontSize: '18px',
-      };
-
-      return (
-        <div style={displayContainerStyle} key={i.toString()}>
-          <Tooltip
-            appearance={isDark ? 'dark' : 'light'}
-            key={i.toString()}
-            {...tooltipProps}
-          >
-            {tooltipProps.children}
-          </Tooltip>
-        </div>
-      );
-    });
-
-    const title = state.charAt(0).toUpperCase() + state.slice(1);
-
-    return (
-      <section className="isb-section" key={index.toString()}>
-        <h2>{title}</h2>
-        <div className="isb-section__content">{list}</div>
-      </section>
-    );
-  });
-
-  return <>{component}</>;
-};
-
-const demoReactComponent = (
-  <div>
-    <span
-      style={{ color: '#30C9AD', fontSize: 12, textTransform: 'uppercase' }}
-    >
-      Strongly Favorable
-    </span>
-    <br />
-    <span style={{ fontWeight: 'bold', fontSize: 32 }}>
-      100,0<sup style={{ fontSize: 14 }}>%</sup>
-    </span>
-  </div>
+export const Overview = () => (
+  <Tooltip content={tooltipContent}>Playground tooltip</Tooltip>
 );
 
-const tooltipStates: TooltipProps[] = [
-  {
-    children: 'Default',
-    content: tooltipContent,
-  },
-  {
-    children: 'On Top',
-    content: tooltipContent,
-    position: 'top',
-  },
-  {
-    children: 'On the right',
-    content: tooltipContent,
-    position: 'right',
-  },
-  {
-    children: 'Underneath',
-    content: tooltipContent,
-    position: 'bottom',
-  },
-  {
-    children: 'On the left',
-    content: tooltipContent,
-    position: 'left',
-  },
-  {
-    children: 'With ReactComponent',
-    content: demoReactComponent,
-    position: 'bottom',
-  },
-];
+export const Appearances = () => (
+  <Section>
+    <Tooltip content={tooltipContent} appearance="dark">
+      Dark
+    </Tooltip>
+    <Tooltip content={tooltipContent} appearance="light">
+      Light
+    </Tooltip>
+  </Section>
+);
 
-export const Standard = (): React.ReactElement => (
-  <Template payload={{ dark: tooltipStates, light: tooltipStates }} />
+export const Position = () => (
+  <Section>
+    <Tooltip content={tooltipContent} position="top">
+      Top
+    </Tooltip>
+    <Tooltip content={tooltipContent} position="right">
+      Right
+    </Tooltip>
+    <Tooltip content={tooltipContent} position="bottom">
+      Bottom
+    </Tooltip>
+    <Tooltip content={tooltipContent} position="left">
+      Left
+    </Tooltip>
+  </Section>
 );
