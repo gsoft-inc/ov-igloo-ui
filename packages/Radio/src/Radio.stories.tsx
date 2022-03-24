dd1d3c3e83b54798888c9c462004cf075cd81108
Franck Gaudin <franck.gaudin@gsoft.com>
2022-03-24 15:06:16 -0400

    feat(radio) (#92)
    * feat(radio): Initial commit
    
    * feat(Message): fix radio size
    
    * feat(Message): update code after review comments
    
    * feat(Radio): update content of changeset
    
    * feat(Radio): fix style link on package.json

---
+++ packages/Radio/src/Radio.stories.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import Section from '@components/section';
+import readme from '../README.md';
+
+import Radio from './Radio';
+
+export default {
+  title: 'Components/Radio',
+  component: Radio,
+  parameters: {
+    description: readme,
+  },
+  argTypes: {
+    disabled: { table: { defaultValue: { summary: false } } },
+    checked: { table: { defaultValue: { summary: false } } },
+    small: { table: { defaultValue: { summary: false } } },
+  },
+} as ComponentMeta<typeof Radio>;
+
+const Template: ComponentStory<typeof Radio> = (args) => <Radio {...args} />;
+export const Overview = Template.bind({});
+Overview.args = {
+  htmlFor: 'ids-radio',
+  children: 'Label',
+};
+
+export const Checked: React.VFC<unknown> = () => (
+  <Radio htmlFor="ids-radio-active" checked>
+    Label
+  </Radio>
+);
+
+export const Disabled: React.VFC<unknown> = () => (
+  <Section>
+    <Radio htmlFor="ids-radio-disabled" disabled>
+      Disabled
+    </Radio>
+    <Radio htmlFor="ids-radio-disabled-active" checked disabled>
+      Checked and disabled
+    </Radio>
+  </Section>
+);
+
+export const Small: React.VFC<unknown> = () => (
+  <Radio htmlFor="ids-radio-small" small>
+    Small
+  </Radio>
+);
+
+export const WithHelperText: React.VFC<unknown> = () => (
+  <Radio htmlFor="ids-radio-description" helperText="Helper text">
+    Label
+  </Radio>
+);
