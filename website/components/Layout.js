8ba323d88b782dfeb56cae34d740e2726ebf85d0
Franck <franck.gaudin@gsoft.com>
2022-04-22 21:45:33 -0400

    chore(Docs): refactor Navbar

--- website/components/Layout.js
+++ website/components/Layout.js
@@ -1,9 +1,33 @@
 import Head from 'next/head';
+
 import Header from './Header';
 import Nav from './Nav';
 import { nav } from '../navigation';
 
+import brand from '../svg/brand.svg';
+import github from '../svg/github.svg';
+import slack from '../svg/slack-monochrome.svg';
+import darkMode from '../svg/dark-mode.svg';
+
 export default function Layout({ children }) {
+  const externalLink = [
+    {
+      href: 'https://github.com/gsoft-inc/ov-igloo-ui',
+      icon: github,
+      label: 'GitHub',
+    },
+    {
+      href: 'https://github.com/gsoft-inc/ov-igloo-ui',
+      icon: slack,
+      label: 'Slack',
+    },
+    {
+      href: '#',
+      icon: darkMode,
+      label: 'Dark mode',
+    },
+  ];
+
   return (
     <>
       <Head>
@@ -11,7 +35,7 @@ export default function Layout({ children }) {
         <link rel="icon" type="image/png" href="/igloo.svg" />
       </Head>
       <div className="io-layout">
-        <Header>
+        <Header brand={brand} links={externalLink}>
           <Nav items={nav} />
         </Header>
         <main className="io-main">{children}</main>
