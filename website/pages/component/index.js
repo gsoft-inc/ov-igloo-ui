e86680f02df4d17ffe18008e7f10adcd2ab4d7bf
Franck <franck.gaudin@gsoft.com>
2022-05-17 20:18:28 -0400

    chore(docs): integration of components hub

--- website/pages/component/index.js
+++ website/pages/component/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Image from 'next/image';
 import Display from '../../components/Display';
 import Card from '../../components/Card';
 
@@ -6,12 +7,18 @@ import { COMPONENTS_SOURCE } from './[slug]';
 
 import iconIcon from '../../svg/component.svg';
 import { generateComponentList } from '../../scripts/generate-component-list';
+import { getCardImage } from '../../scripts/files-utils';
 
 export default function Components(props) {
-  const { components } = props;
+  const { components, images } = props;
+
   const componentsList = components.map((component, index) => {
+    const image = images.filter((image) => image[component])[0][component];
+    const cardImage = require(`../../svg/components/${image}`);
+
     return (
       <Card
+        icon={<Image src={cardImage} />}
         key={index.toString()}
         component
         title={component}
@@ -24,11 +31,13 @@ export default function Components(props) {
       <Head>
         <title>Components - Igloo</title>
       </Head>
-      <section className="io-section io-section--tight io-container">
+      <section className="io-section io-container">
         <Display icon={iconIcon}>
           <div className="io-display__content">Components</div>
         </Display>
-        <div className="io-section__content">{componentsList}</div>
+        <div className="io-section__content io-section__content--hub">
+          {componentsList}
+        </div>
       </section>
     </>
   );
@@ -36,9 +45,12 @@ export default function Components(props) {
 
 export const getStaticProps = async () => {
   const components = generateComponentList(COMPONENTS_SOURCE);
+  const cardImage = await getCardImage(components);
+
   return {
     props: {
       components,
+      images: cardImage,
     },
   };
 };
