e799b4d1136f7c57cdb3b8902726ff16652de04c
Alexandre Beaudoin <alexandre.beaudoin@gsoft.com>
2022-06-06 13:31:11 -0400

    Liens vers une icône précise depuis la page des icônes (#145)
    * Fix command execution on Windows and code highlight
    
    * WIP
    
    * Optimisation de l'import de Prism
    
    * Focus sur l'icône lorsque l'on clique dessus
    
    * Modifier la commande doc:build pour quelle fonctionne sur Windows

--- website/components/IconsList.js
+++ website/components/IconsList.js
@@ -1,4 +1,8 @@
+import { useRouter } from 'next/router';
 import * as Icon from '@igloo-ui/icons/iconsList';
+import Link from '@igloo-ui/icons/dist/Link';
+
+import ClipboardCopy from './ClipboardCopy';
 
 export default function IconsList({
   options,
@@ -8,18 +12,54 @@ export default function IconsList({
   emptyResult,
 }) {
   const searching = search.length > 0;
+  const router = useRouter();
+
+  function generateCodeSnippet(name, size) {
+    return `import ${name} from '@igloo-ui/icons/dist/${name}';${'\n'}<${name} size="${size}" />`;
+  }
+
+  function generateShareLink(name, size) {
+    const basePath =
+      typeof window !== 'undefined'
+        ? location.protocol + '//' + location.host
+        : router.basePath;
+    const pathname = router.pathname;
+
+    return `${basePath}${pathname}?search=%3D${name}&size=${size}`;
+  }
+
+  function setSearch(name, size) {
+    router.push(`?search=%3D${name}&size=${size}`, undefined, {
+      shallow: true,
+    });
+  }
 
   const Item = ({ index, name, size }) => {
     const IconComponent = Icon[name];
     return (
       <div key={index.toString()} className="io-icon">
         <div className="io-icon__content">
-          <div className="io-icon__svg">
+          <div
+            className="io-icon__svg"
+            onClick={() => {
+              setSearch(name, size);
+            }}
+          >
             <IconComponent size={size} />
           </div>
           <div className="io-icon__name" title={name}>
             {name}
           </div>
+          <ClipboardCopy
+            className="io-highlighter__action-left"
+            textToCopy={generateShareLink(name, size)}
+            icon={<Link />}
+            left
+          />
+          <ClipboardCopy
+            className="io-highlighter__action"
+            textToCopy={generateCodeSnippet(name, size)}
+          />
         </div>
       </div>
     );
