ce667d38d1383b6faf4dbc28ce77d0364c5709fc
Franck <franck.gaudin@gsoft.com>
2022-08-01 15:24:47 +0200

    feat(popover): add tests

--- packages/Popover/src/Popover.test.tsx
+++ packages/Popover/src/Popover.test.tsx
@@ -2,18 +2,50 @@
  * @jest-environment jsdom
  */
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render, fireEvent, screen } from '@testing-library/react';
 
-import Popover from './Popover';
+import Popover, { PopoverProps } from './Popover';
+
+const setUp = (props: PopoverProps) => {
+  return render(<Popover {...props}>popover trigger</Popover>);
+};
 
 describe('Popover', () => {
-  const component = shallow(<Popover>Hello world</Popover>);
-  test('It should render without errors', () => {
-    const wrapper = component.find('.ids-popover');
-    expect(wrapper.length).toBe(1);
+  let component: any;
+  const defaultComponentSetUp = {
+    children: 'trigger',
+    content: 'Popover copy',
+    dataTest: 'ids-popover-id',
+  };
+
+  test('It should render a snapshot', (): void => {
+    component = setUp(defaultComponentSetUp);
+    expect(component).toMatchSnapshot();
   });
 
-  test('It should render a snapshot', () => {
-    expect(component).toMatchSnapshot();
+  test('It should render without errors', (): void => {
+    const { getByTestId } = setUp(defaultComponentSetUp);
+
+    fireEvent.click(screen.getByText(/trigger/));
+    expect(getByTestId('ids-popover-id')).toBeInTheDocument();
+  });
+
+  test('It should render a title', (): void => {
+    component = setUp({ ...defaultComponentSetUp, title: 'Date' });
+    const { getByText } = component;
+
+    fireEvent.click(screen.getByText(/trigger/));
+    expect(getByText(/Date/)).toBeInTheDocument();
+  });
+
+  test('It should render a action', (): void => {
+    component = setUp({
+      ...defaultComponentSetUp,
+      action: 'learn more',
+    });
+    const { getByText } = component;
+
+    fireEvent.click(screen.getByText(/trigger/));
+    expect(getByText(/learn more/)).toBeInTheDocument();
   });
 });
