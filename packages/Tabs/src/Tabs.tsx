8af1c9501582ea7936aa62e6c1b8255140059782
vicky-comeau <vicky.comeau@gsoft.com>
2022-08-29 16:35:37 -0400

    Fixed comments and removed ones that were not needed

--- packages/Tabs/src/Tabs.tsx
+++ packages/Tabs/src/Tabs.tsx
@@ -8,7 +8,7 @@ import TextBulletSolid from '@igloo-ui/icons/dist/TextBulletSolid';
 import './tabs.scss';
 
 export interface TabInterface {
-  /** The tab pane contents of the tabs list */
+  /** The contents of the tabs list */
   children?: React.ReactNode;
   /** Add an id attribute to the tab */
   id?: string;
@@ -23,13 +23,13 @@ export interface TabInterface {
 export interface TabsProps extends React.ComponentProps<'div'> {
   /** The function that's called when a tab is clicked */
   onSelectTab?: (index: number) => void;
-  /** The index number of the Tab that should be selected on load */
+  /** The index number of the tab that should be selected on load */
   selected?: number;
   /** Add a data-test tag for automated tests */
   dataTest?: string;
-  /** Add a specific class to the Tabs component */
+  /** Add a specific class to the tabs component */
   className?: string;
-  /** Add an inline style to the Tabs */
+  /** Add an inline style to the tabs */
   isInline?: boolean;
   /** An array of objects that specify the props of the tab buttons */
   tabs: Array<TabInterface>;
@@ -48,14 +48,12 @@ const Tabs: React.FunctionComponent<TabsProps> = ({
     'ids-tabs--heading': !isInline,
   });
 
-  /* Handle the on click */
   const handleOnClick = (index: number): void => {
     if (onSelectTab) {
       onSelectTab(index);
     }
   };
 
-  /* Render the Tab */
   const renderTabItem = (tab: TabInterface, index: number): React.ReactNode => {
     const isTypeString = typeof tab.label === 'string';
     let tabItem = (
@@ -103,7 +101,6 @@ const Tabs: React.FunctionComponent<TabsProps> = ({
         {tabs.map((tab, index) => renderTabItem(tab, index))}
       </ul>
 
-      {/* show selcted tab by index */}
       {tabs[selected].children && tabs[selected].children}
     </div>
   );
