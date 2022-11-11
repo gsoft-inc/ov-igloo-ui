d7186bfc3aa904018f11858b633983d9b8f44aab
Franck <franck.gaudin@gsoft.com>
2022-11-11 09:02:35 -0500

    feat(ProgressBar): update test with @testing-library

--- packages/ProgressBar/src/ProgressBar.test.tsx
+++ packages/ProgressBar/src/ProgressBar.test.tsx
@@ -2,18 +2,16 @@
  * @jest-environment jsdom
  */
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render, screen } from '@testing-library/react';
 
 import ProgressBar from './ProgressBar';
 
 describe('ProgressBar', () => {
-  const component = shallow(<ProgressBar value={0.7} />);
-  test('It should render without errors', () => {
-    const wrapper = component.find('.ids-progress-bar');
-    expect(wrapper.length).toBe(1);
-  });
-
-  test('It should render a snapshot', () => {
-    expect(component).toMatchSnapshot();
+  test('It should render without errors and a snapshot', () => {
+    const { asFragment } = render(
+      <ProgressBar value={0.5} dataTest="ids-progress-bar" />
+    );
+    expect(screen.getByTestId('ids-progress-bar')).toBeInTheDocument();
+    expect(asFragment()).toMatchSnapshot();
   });
 });
