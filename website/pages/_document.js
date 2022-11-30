622e327fc641b43052b15cd2cde2489d1eca5156
Franck <franck.gaudin@gsoft.com>
2022-11-30 09:34:25 -0500

    chore: add config for Google Tag Manager

---
+++ website/pages/_document.js
@@ -0,0 +1,24 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+import { GTM_ID } from '../lib/gtm';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <title>Igloo - Design System Doc</title>
+      </Head>
+      <body>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
