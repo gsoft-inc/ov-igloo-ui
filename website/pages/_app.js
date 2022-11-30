37d7028c67801f51947966db11e036b5fe582747
Franck Gaudin <franck.gaudin@gsoft.com>
2022-11-30 13:37:31 -0500

    chore: replace Google Analytics config by Google Tag Manager  (#272)
    * chore: add config for Google Tag Manager
    
    * chore: remove the `Head` tag from `_document`
    
    * chore: rollback replace the `Head` tag from `_document`
    
    * chore: rollback replace the `Head` tag from `_document`
    
    * chore: rollback replace the `Head` tag from `_document`

--- website/pages/_app.js
+++ website/pages/_app.js
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import Script from 'next/script';
 import { useRouter } from 'next/router';
 
-import * as ga from '../lib/ga';
+import { GTM_ID, pageview } from '../lib/gtm';
 
 import Layout from '../components/Layout';
 
@@ -12,36 +12,25 @@ function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
   useEffect(() => {
-    const handleRouteChange = (url) => {
-      ga.pageview(url);
-    };
-
-    router.events.on('routeChangeComplete', handleRouteChange);
-
+    router.events.on('routeChangeComplete', pageview);
     return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
+      router.events.off('routeChangeComplete', pageview);
     };
   }, [router.events]);
 
   return (
     <>
       <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${ga.GA_TRACKING_ID}`}
-      />
-      <Script
-        id="google-analytics"
+        id="gtag-base"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-
-              gtag('config', '${ga.GA_TRACKING_ID}', {
-                page_path: window.location.pathname,
-              });
-            `,
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', '${GTM_ID}');
+          `,
         }}
       />
       <Layout page={router.asPath}>
---
+++ website/pages/_document.js
@@ -0,0 +1,21 @@
+import { Html, Main, Head, NextScript } from 'next/document';
+import { GTM_ID } from '../lib/gtm';
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <body>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
