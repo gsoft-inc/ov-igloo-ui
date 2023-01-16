fa3812b6bd693abc6a0c97b4edf9e8a3a4456194
vicky-comeau <vicky.comeau@gsoft.com>
2023-01-16 14:12:56 -0500

    Fix: [OV-41094] Added locale prop to AreaChart

--- packages/AreaChart/src/AreaChart.tsx
+++ packages/AreaChart/src/AreaChart.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import cx from 'classnames';
-import { DateTime } from 'luxon';
+import { DateTime, Settings } from 'luxon';
 import {
   AreaChart as RechartsAreaChart,
   Area,
@@ -77,6 +77,7 @@ export interface AreaChartProps extends React.ComponentProps<'div'> {
   dateRange: DateTimeRange;
   /** Whether or not the chart should resize */
   isResponsive?: boolean;
+  locale?: string;
   /** The min and max value of the y axis
    * (Possible values: number, 'auto', 'dataMin' or 'dataMax')
    */
@@ -103,6 +104,7 @@ const AreaChart: React.FunctionComponent<AreaChartProps> = (
     dateRange,
     loading = false,
     isResponsive = true,
+    locale = 'en',
     range = { max: 'auto', min: 0 },
     scoreFormatter,
     tooltipScoreFormatter,
@@ -113,6 +115,8 @@ const AreaChart: React.FunctionComponent<AreaChartProps> = (
   const TICK_LIMIT = 7;
   const DEFAULT_SKELETON_WIDTH = 24;
   const DEFAULT_SKELETON_HEIGHT = 8;
+
+  Settings.defaultLocale = locale;
 
   const SkeletonAxisTick = ({
     x,
@@ -128,7 +132,7 @@ const AreaChart: React.FunctionComponent<AreaChartProps> = (
     payload?: { offset: number };
     skeletonWidth?: number;
     skeletonHeight?: number;
-  }) => {
+  }): React.ReactElement => {
     let positionX;
     let positionY;
 
