d0b26f26be62dd98f231bdfd2af97a89a6f2b88c
Franck <franck.gaudin@gsoft.com>
2023-04-06 13:50:11 -0400

    test: add interaction test for the focus on Hyperlink
    OV-42054

--- packages/Hyperlink/src/Hyperlink.stories.tsx
+++ packages/Hyperlink/src/Hyperlink.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-import { fireEvent, userEvent, within } from '@storybook/testing-library';
+import { userEvent, within } from '@storybook/testing-library';
 import { expect } from '@storybook/jest';
 
 import readme from '../README.md';
