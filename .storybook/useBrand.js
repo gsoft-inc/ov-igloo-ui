55aea2d72b348f64870e35d413fd2f671cf32207
Franck <franck.gaudin@gsoft.com>
2023-09-11 14:47:03 -0400

    Feat: Checkbox rebrand #OV-46007

--- .storybook/useBrand.js
+++ .storybook/useBrand.js
@@ -1,41 +1,42 @@
 import { useEffect } from 'react';
 
 export const useBrand = (StoryFn, context) => {
-  const {brand} = context.globals;
-  const {displayName} = context.component;
-  const components = [
-    "ActionMenu",
-    "Alert",
-    "AreaChart",
-    "Avatar",
-    "BarChart",
-    "Button",
-    "Breadcrumb",
-    "Card",
-    "Color",
-    "Hyperlink",
-    "IconButton",
-    "List",
-    "Metric",
-    "PieChart",
-    "Popover",
-    "StackedBar",
-    "Tag",
-    "Textarea",
-    "Toggle",
-    "Tooltip",
-    "VerticalBarChart",
-    "VisualIdentifier"
-  ];
+    const {brand} = context.globals;
+    const {displayName} = context.component;
+    const components = [
+        "ActionMenu",
+        "Alert",
+        "AreaChart",
+        "Avatar",
+        "BarChart",
+        "Button",
+        "Breadcrumb",
+        "Checkbox",
+        "Card",
+        "Color",
+        "Hyperlink",
+        "IconButton",
+        "List",
+        "Metric",
+        "PieChart",
+        "Popover",
+        "StackedBar",
+        "Tag",
+        "Textarea",
+        "Toggle",
+        "Tooltip",
+        "VerticalBarChart",
+        "VisualIdentifier"
+    ];
 
-  useEffect(() => {
-    //document.documentElement refers to html tag inside iframe#storybook-preview-iframe
-    if (components.includes(displayName)) {
-      document.documentElement.setAttribute('data-brand', brand);
-    } else {
-      document.documentElement.removeAttribute('data-brand');
-    }
-  }, [brand]);
+    useEffect(() => {
+        //document.documentElement refers to html tag inside iframe#storybook-preview-iframe
+        if (components.includes(displayName)) {
+            document.documentElement.setAttribute('data-brand', brand);
+        } else {
+            document.documentElement.removeAttribute('data-brand');
+        }
+    }, [brand]);
 
-  return StoryFn();
+    return StoryFn();
 }
