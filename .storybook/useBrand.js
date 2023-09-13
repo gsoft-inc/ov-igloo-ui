278a806588fef3f3f54e62c5f4268bbad059a613
Francis Thibault <francis.thibault@gmail.com>
2023-09-13 13:08:46 -0400

    rebrand pager

--- .storybook/useBrand.js
+++ .storybook/useBrand.js
@@ -17,6 +17,7 @@ export const useBrand = (StoryFn, context) => {
     "IconButton",
     "List",
     "Metric",
+    "Pager",
     "PieChart",
     "Popover",
     "StackedBar",
--- packages/Pager/src/svgs.tsx
+++ packages/Pager/src/svgs.tsx
@@ -1,25 +1,25 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React from "react";
 
 export const EllipsisIcon: React.FunctionComponent = () => (
-  <svg
-    width="12"
-    height="12"
-    viewBox="0 0 12 12"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      d="M6 7C6.55228 7 7 6.55228 7 6C7 5.44772 6.55228 5 6 5C5.44772 5 5 5.44772 5 6C5 6.55228 5.44772 7 6 7Z"
-      fill="#606B79"
-    />
-    <path
-      d="M2 7C2.55228 7 3 6.55228 3 6C3 5.44772 2.55228 5 2 5C1.44772 5 1 5.44772 1 6C1 6.55228 1.44772 7 2 7Z"
-      fill="#606B79"
-    />
-    <path
-      d="M10 7C10.5523 7 11 6.55228 11 6C11 5.44772 10.5523 5 10 5C9.44772 5 9 5.44772 9 6C9 6.55228 9.44772 7 10 7Z"
-      fill="#606B79"
-    />
-  </svg>
+    <svg
+        width="12"
+        height="12"
+        viewBox="0 0 12 12"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path
+            d="M6 7C6.55228 7 7 6.55228 7 6C7 5.44772 6.55228 5 6 5C5.44772 5 5 5.44772 5 6C5 6.55228 5.44772 7 6 7Z"
+            fill="currentColor"
+        />
+        <path
+            d="M2 7C2.55228 7 3 6.55228 3 6C3 5.44772 2.55228 5 2 5C1.44772 5 1 5.44772 1 6C1 6.55228 1.44772 7 2 7Z"
+            fill="currentColor"
+        />
+        <path
+            d="M10 7C10.5523 7 11 6.55228 11 6C11 5.44772 10.5523 5 10 5C9.44772 5 9 5.44772 9 6C9 6.55228 9.44772 7 10 7Z"
+            fill="currentColor"
+        />
+    </svg>
 );
