24146bcb46f46c4e61ece3ddb38d7aef64228dfd
Franck Gaudin <franck.gaudin@gsoft.com>
2023-09-20 09:33:40 -0400

    Feat(rebrand): Gave the FormGroup component the new Brand. (#543)
    Ticket: IGL-62

--- .storybook/useBrand.js
+++ .storybook/useBrand.js
@@ -16,6 +16,7 @@ export const useBrand = (StoryFn, context) => {
         "Card",
         "Color",
         "Dropdown",
+        "FormGroup",
         "Hyperlink",
         "HelperText",
         "IconButton",
