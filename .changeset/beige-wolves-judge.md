6c3d381ca9950c5772b4c4cadb9352ac021a3ec2
vicky-comeau <vicky.comeau@gsoft.com>
2023-11-03 16:20:01 -0400

    Feat: [IGL-95] Filter rebrand

---
+++ .changeset/beige-wolves-judge.md
@@ -0,0 +1,10 @@
+---
+"@igloo-ui/filter": minor
+"@igloo-ui/area-chart": patch
+"@igloo-ui/pie-chart": patch
+"@igloo-ui/stacked-bar": patch
+"@igloo-ui/vertical-bar-chart": patch
+"docs": patch
+---
+
+Updated storybook and recharts. Applied the new brand look for filter.
--- .storybook/useBrand.js
+++ .storybook/useBrand.js
@@ -2,62 +2,10 @@ import { useEffect } from 'react';
 
 export const useBrand = (StoryFn, context) => {
     const {brand} = context.globals;
-    const {displayName} = context.component;
-    const components = [
-        "ActionMenu",
-        "Alert",
-        "AreaChart",
-        "Avatar",
-        "BarChart",
-        "Button",
-        "ButtonGroup",
-        "Breadcrumb",
-        "Carousel",
-        "Checkbox",
-        "Card",
-        "Color",
-        "Combobox",
-        "Datepicker",
-        "Dialog",
-        "Disclosure",
-        "Dropdown",
-        "Ellipsis",
-        "FormGroup",
-        "Hyperlink",
-        "HelperText",
-        "IconButton",
-        "Input",
-        "List",
-        "Metric",
-        "Modal",
-        "OptionButton",
-        "Pager",
-        "PieChart",
-        "Popover",
-        "ProgressBar",
-        "Radio",
-        "Select",
-        "StackedBar",
-        "Tabs",
-        "Stepper",
-        "Tag",
-        "TagPicker",
-        "Textarea",
-        "TextEditor",
-        "Toast",
-        "Toggle",
-        "Tooltip",
-        "VerticalBarChart",
-        "VisualIdentifier"
-    ];
 
     useEffect(() => {
         //document.documentElement refers to html tag inside iframe#storybook-preview-iframe
-        if (components.includes(displayName)) {
-            document.documentElement.setAttribute('data-brand', brand);
-        } else {
-            document.documentElement.removeAttribute('data-brand');
-        }
+        document.documentElement.setAttribute('data-brand', brand);
     }, [brand]);
 
     return StoryFn();
