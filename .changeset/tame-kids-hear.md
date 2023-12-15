c939498253b2a4e46e52ad6e84156088e0349f5b
Franck Gaudin <franck.gaudin@workleap.com>
2023-12-15 15:06:37 -0500

    fix(ButtonGroup): fix component rendering when used with a tooltip
    add a data tag to the first and last elements to style borders radius

---
+++ .changeset/tame-kids-hear.md
@@ -0,0 +1,5 @@
+---
+"@igloo-ui/button-group": patch
+---
+
+fix component rendering when used with a tooltip
--- packages/ButtonGroup/src/ButtonGroup.tsx
+++ packages/ButtonGroup/src/ButtonGroup.tsx
@@ -6,38 +6,55 @@ import './button-group.scss';
 import ButtonGroupItem from './ButtonGroupItem';
 
 export interface ButtonGroupProps {
-  /** Changes the padding of button group */
-  compact?: boolean;
-  /** Changes the size of button group */
-  small?: boolean;
-  /** React child node representing the button content text or icon */
-  children: React.ReactNode;
-  /** Add a data-test tag for automated tests */
-  dataTest?: string;
-  /** Add a specific class to the button group */
-  className?: string;
+    /** Changes the padding of button group */
+    compact?: boolean;
+    /** Changes the size of button group */
+    small?: boolean;
+    /** React child node representing the button content text or icon */
+    children: React.ReactNode;
+    /** Add a data-test tag for automated tests */
+    dataTest?: string;
+    /** Add a specific class to the button group */
+    className?: string;
 }
 
 const ButtonGroup: React.FunctionComponent<ButtonGroupProps> = ({
-  children,
-  compact = false,
-  small = false,
-  className,
-  dataTest,
-  ...rest
+    children,
+    compact = false,
+    small = false,
+    className,
+    dataTest,
+    ...rest
 }: ButtonGroupProps) => {
-  return (
-    <div
-      data-test={dataTest}
-      className={cx('ids-btn-group', className, {
-        'ids-btn-group--small': small,
-        'ids-btn-group--compact': compact,
-      })}
-      {...rest}
-    >
-      {children}
-    </div>
-  );
+    let childrenClone = React.Children.toArray(children);
+
+    childrenClone = childrenClone.map((child, index) => {
+        if (React.isValidElement(child)) {
+            return React.cloneElement(child, {
+                ...child.props,
+                ["data-item"]: cx({
+                    'is-first': index === 0,
+                    'is-last': index === childrenClone.length - 1,
+                }),
+            });
+        }
+        return child;
+    });
+
+    console.log(childrenClone);
+
+    return (
+        <div
+            data-test={dataTest}
+            className={cx('ids-btn-group', className, {
+                'ids-btn-group--small': small,
+                'ids-btn-group--compact': compact,
+            })}
+            {...rest}
+        >
+            {childrenClone}
+        </div>
+    );
 };
 
 export { ButtonGroupItem as ButtonItem };
