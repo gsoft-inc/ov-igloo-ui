c3d85deed25a6c97bd963e7f837ed05e0eef4bb0
vicky-comeau <vicky.comeau@gsoft.com>
2024-01-31 15:06:08 -0500

    Fix: [IGL-121] Select and Combobox chevrons

---
+++ .changeset/thin-llamas-vanish.md
@@ -0,0 +1,6 @@
+---
+"@igloo-ui/combobox": patch
+"@igloo-ui/select": patch
+---
+
+Update the chevron icon for Select and Combobox. Updated the clear icon as well and adjusted spacing.
--- packages/Select/src/SelectInput.tsx
+++ packages/Select/src/SelectInput.tsx
@@ -1,35 +1,49 @@
-import * as React from 'react';
+import * as React from "react";
 
-import ChevronUp from '@igloo-ui/icons/dist/TableChevronSelectedUp';
-import ChevronDown from '@igloo-ui/icons/dist/TableChevronSelectedDown';
+import ChevronUp from "@igloo-ui/icons/dist/TableChevronSelectedUp";
+import ChevronDown from "@igloo-ui/icons/dist/TableChevronSelectedDown";
+import { AngleDownIcon, AngleUpIcon } from "@hopper-ui/icons-react16";
 
-import './select-input.scss';
+import "./select-input.scss";
 
-export interface SelectInputProps extends React.ComponentProps<'div'> {
-  /** Default value displayed in the Select Input */
-  children?: React.ReactNode;
-  /** True if the option list is displayed */
-  isOpen?: boolean;
-  /** Callback for when the clear button is clicked */
+export interface SelectInputProps extends React.ComponentProps<"div"> {
+    /** Default value displayed in the Select Input */
+    children?: React.ReactNode;
+    /** True if the option list is displayed */
+    isOpen?: boolean;
+    /** Callback for when the clear button is clicked */
 }
 
+const getBrand = (): string => {
+    return document.documentElement.getAttribute("data-brand") ?? "igloo";
+};
+
 const SelectInput: React.FunctionComponent<SelectInputProps> = ({
-  children,
-  isOpen = false,
-  ...rest
+    children,
+    isOpen = false,
+    ...rest
 }: SelectInputProps) => {
-  return (
-    <div className="ids-select__input" {...rest}>
-      {children}
-      <div className="ids-select__input-actions">
-        {isOpen ? (
-          <ChevronUp className="ids-select__input__chevron" size="small" />
-        ) : (
-          <ChevronDown className="ids-select__input__chevron" size="small" />
-        )}
-      </div>
-    </div>
-  );
+    const isWorkleap = getBrand() === "workleap";
+    const chevronClass = "ids-select__input__chevron";
+    const chevronUpIcon = isWorkleap ?
+        <AngleUpIcon className={chevronClass} size="sm" /> : 
+        <ChevronUp className={chevronClass} size="small" />;
+    const chevronDownIcon = isWorkleap ? 
+        <AngleDownIcon className={chevronClass} size="sm" /> : 
+        <ChevronDown className={chevronClass} size="small" />;
+
+    return (
+        <div className="ids-select__input" {...rest}>
+            {children}
+            <div className="ids-select__input-actions">
+                {isOpen ? (
+                    chevronUpIcon
+                ) : (
+                    chevronDownIcon
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default SelectInput;
