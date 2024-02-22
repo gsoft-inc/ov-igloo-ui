c5e01d408ba81f3be84265183a437518988c0418
Alexandre Asselin <alexandre.asselin@gsoft.com>
2024-02-22 14:01:25 -0500

    remove scss

--- packages/Provider/src/Provider.tsx
+++ packages/Provider/src/Provider.tsx
@@ -2,19 +2,17 @@ import * as React from "react";
 
 import { I18nProvider, type I18nProviderProps, useLocale, useLocalizedStringFormatter } from "react-aria";
 
-import "./provider.scss";
-
 export type AvailableLocales = "en-US" | "fr-CA";
 
 export interface IglooProviderProps extends Omit<I18nProviderProps, "locale"> {
     locale: AvailableLocales;
 }
-const IglooProvider: React.FunctionComponent<IglooProviderProps> = props => { 
+const IglooProvider: React.FunctionComponent<IglooProviderProps> = props => {
     return (
         <I18nProvider {...props} />
     );
 };
 
-export { useLocale, useLocalizedStringFormatter }; 
+export { useLocale, useLocalizedStringFormatter };
 
 export default IglooProvider;
